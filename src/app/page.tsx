4f8630a3b9c564b824da1bec1106f75504b22c24
tomichans <kaito39smile@gmail.com>
2023-05-21 10:53:08 +0900

    ✨ ヘッダー、メイン、フッターコンポーネントをページに追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import Head from 'next/head'
+import Footer from './components/Footer'
+import Header from './components/Header'
 import styles from './page.module.css'
+import Main from './components/Main'
 
 export default function Home() {
-  return <h1 className={styles.hello}>Hello, World</h1>
+  return (
+    <>
+      <Head>
+        <title>家計簿アプリ</title>
+      </Head>
+      <Header />
+      <Main />
+      <h1 className={styles.hello}>Hello, World</h1>
+      <Footer />
+    </>
+  ) 
 }
