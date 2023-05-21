4bba09373c026a5b31070e31b4b738e04d9c954b
tomichans <kaito39smile@gmail.com>
2023-05-21 22:03:40 +0900

    ♻️ layout.tsxにヘッダーとフッターを追加してすべての画面で表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Head from 'next/head'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import styles from './page.module.css'
-import Main from './components/Main'
+import Main from './Main/page'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>家計簿アプリ</title>
-      </Head>
-      <Header />
       <Main />
-      <h1 className={styles.hello}>Hello, World</h1>
-      <Footer />
     </>
-  ) 
+  )
 }
