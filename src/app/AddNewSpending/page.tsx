782332706671655baae4a4dc7e02afffbbc2eec5
tomichans <kaito39smile@gmail.com>
2023-06-12 22:46:24 +0900

    ♻️ 支出入力画面のUIを調整

--- src/app/AddNewSpending/page.tsx
+++ src/app/AddNewSpending/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 import {
+  Box,
   Button,
-  Flex,
+  Text,
+  HStack,
   Heading,
   Input,
   ListItem,
@@ -9,6 +11,7 @@ import {
 } from '@chakra-ui/react'
 import { useAddSpending } from '../../../hooks/useAddSpending'
 
+// 支出入力画面
 export default function AddNewSpending() {
   const {
     transactions,
@@ -24,32 +27,63 @@ export default function AddNewSpending() {
   } = useAddSpending()
 
   return (
-    <>
-      <Heading fontSize="5xl">家計簿アプリ</Heading>
-      <Flex>
+    <Box p="6" height="50%" w="60%" bgColor="white">
+      <Heading fontSize="4xl" mb="8">
+        家計簿を入力
+      </Heading>
+      <HStack>
+        <Text px="4" w="20%" fontSize="2xl">
+          品目名
+        </Text>
+        <Text px="4" w="20%" fontSize="2xl">
+          カテゴリ
+        </Text>
+        <Text px="4" w="20%" fontSize="2xl">
+          金額
+        </Text>
+        <Text px="4" w="20%" fontSize="2xl">
+          メモ
+        </Text>
+      </HStack>
+      <HStack>
         <Input
-          w="300"
+          w="20%"
+          type="text"
+          placeholder="品目名"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <Input
+          w="20%"
           type="text"
           placeholder="カテゴリ"
           value={category}
           onChange={(e) => setCategory(e.target.value)}
         />
         <Input
-          w="300"
+          w="20%"
           type="number"
           placeholder="金額"
           value={amount}
-          onChange={(e) => setAmount(e.target.value)}
+          onChange={(e) => setAmount(parseInt(e.target.value))}
         />
+        <Input
+          w="20%"
+          type="text"
+          placeholder="メモ"
+          value={memo}
+          onChange={(e) => setMemo(e.target.value)}
         />
         <Button onClick={addSpending}>追加</Button>
+      </HStack>
       <UnorderedList>
         {transactions.map((transaction, index) => (
           <ListItem key={index}>
-            カテゴリ: {transaction.category} 金額: {transaction.amount}
+            品目名： {transaction.item} カテゴリ: {transaction.category} 金額:
+            {transaction.amount}
           </ListItem>
         ))}
       </UnorderedList>
-    </>
+    </Box>
   )
 }
