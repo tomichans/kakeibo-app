68d5be5e375fef5e7d6927803cc8af7952cef899
tomichans <kaito39smile@gmail.com>
2023-06-18 23:35:11 +0900

    ♻️ レイアウト調整

--- src/app/AddNewSpending/page.tsx
+++ src/app/AddNewSpending/page.tsx
@@ -21,88 +21,90 @@ export default function AddNewSpending() {
   } = useAddSpending()
 
   return (
-    <Box p="6" w="60%" bgColor="white">
-      <Heading mb="8" fontSize="4xl">
-        家計簿を入力
-      </Heading>
+    <Box p="6" bg="#F5F5F5">
+      <Box p="6" w="60%" bgColor="white">
+        <Heading mb="8" fontSize="4xl">
+          家計簿を入力
+        </Heading>
 
-      {/* ポップアップ */}
-      <Box
-        py="2"
-        w="20%"
-        textAlign="center"
-        fontWeight="bold"
-        fontSize="xl"
-        bg="#99FF33"
-        color="white"
-        display={addSpendFlg ? 'block' : 'none'}
-      >
-        追加しました
-      </Box>
-      <Box
-        py="2"
-        w="50%"
-        textAlign="center"
-        fontWeight="bold"
-        fontSize="xl"
-        bg="red"
-        color="white"
-        display={addErrFlg ? 'block' : 'none'}
-      >
-        登録できる明細データがありません。
-      </Box>
+        {/* ポップアップ */}
+        <Box
+          py="2"
+          w="20%"
+          textAlign="center"
+          fontWeight="bold"
+          fontSize="xl"
+          bg="#99FF33"
+          color="white"
+          display={addSpendFlg ? 'block' : 'none'}
+        >
+          追加しました
+        </Box>
+        <Box
+          py="2"
+          w="50%"
+          textAlign="center"
+          fontWeight="bold"
+          fontSize="xl"
+          bg="red"
+          color="white"
+          display={addErrFlg ? 'block' : 'none'}
+        >
+          登録できる明細データがありません。
+        </Box>
 
-      {/* 支出入力欄 */}
-      <Box my="5">
-        <HStack>
-          <Text px="4" w="20%" fontSize="2xl">
-            品目名
-          </Text>
-          <Text px="4" w="20%" fontSize="2xl">
-            カテゴリ
-          </Text>
-          <Text px="4" w="20%" fontSize="2xl">
-            金額
-          </Text>
-          <Text px="4" w="20%" fontSize="2xl">
-            メモ
-          </Text>
-        </HStack>
-        <HStack>
-          <Input
-            w="20%"
-            type="text"
-            placeholder="品目名"
-            value={item}
-            onChange={(e) => setItem(e.target.value)}
-          />
-          <Input
-            w="20%"
-            type="text"
-            placeholder="カテゴリ"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-          <Input
-            w="20%"
-            type="number"
-            placeholder="金額"
-            value={amount}
-            onChange={(e) => setAmount(parseInt(e.target.value))}
-          />
-          <Input
-            w="20%"
-            type="text"
-            placeholder="メモ"
-            value={memo}
-            onChange={(e) => setMemo(e.target.value)}
-          />
-          <Button onClick={addSpending}>追加</Button>
-        </HStack>
-      </Box>
+        {/* 支出入力欄 */}
+        <Box my="5">
+          <HStack>
+            <Text px="4" w="20%" fontSize="2xl">
+              品目名
+            </Text>
+            <Text px="4" w="20%" fontSize="2xl">
+              カテゴリ
+            </Text>
+            <Text px="4" w="20%" fontSize="2xl">
+              金額
+            </Text>
+            <Text px="4" w="20%" fontSize="2xl">
+              メモ
+            </Text>
+          </HStack>
+          <HStack>
+            <Input
+              w="20%"
+              type="text"
+              placeholder="品目名"
+              value={item}
+              onChange={(e) => setItem(e.target.value)}
+            />
+            <Input
+              w="20%"
+              type="text"
+              placeholder="カテゴリ"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            <Input
+              w="20%"
+              type="number"
+              placeholder="金額"
+              value={amount}
+              onChange={(e) => setAmount(parseInt(e.target.value))}
+            />
+            <Input
+              w="20%"
+              type="text"
+              placeholder="メモ"
+              value={memo}
+              onChange={(e) => setMemo(e.target.value)}
+            />
+            <Button onClick={addSpending}>追加</Button>
+          </HStack>
+        </Box>
 
-      {/* 追加リスト */}
-      <RecentInputList transactions={transactions} />
+        {/* 追加リスト */}
+        <RecentInputList transactions={transactions} />
+      </Box>
     </Box>
   )
 }
