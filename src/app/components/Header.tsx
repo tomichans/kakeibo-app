5b7f2ba197868ab00b397a0015b22d20dd582ec4
tomichans <kaito39smile@gmail.com>
2023-06-27 07:57:15 +0900

    ✨ ログイン後のヘッダー表示を追加し、支出追加画面へのリンクを追加

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,11 @@
 'use client'
+import { useState } from 'react'
 import NextLink from 'next/link'
 import { Box, Flex, Heading, Link as ChakraUILink } from '@chakra-ui/react'
 
 export default function Header() {
+  const [loginFlg, setLoginFlg] = useState(false)
+
   return (
     <Flex
       align="center"
@@ -12,16 +15,42 @@ export default function Header() {
       color="white"
     >
       <Box as="a" href="#home" display="block" textDecoration="none" w="80vm">
-        <Heading as="h1" size="lg">
-          家計簿アプリ（仮）
-        </Heading>
+        <NextLink href={'/'}>
+          <Heading as="h1" size="lg">
+            家計簿アプリ（仮）
+          </Heading>
+        </NextLink>
       </Box>
-      <Flex align="center" fontSize="xl" fontWeight="bold">
+
+      <Flex
+        display={loginFlg ? 'none' : 'flex'}
+        align="center"
+        fontSize="xl"
+        fontWeight="bold"
+      >
         <NextLink href={'#'}>
-          <ChakraUILink mr={7}>Sign-in</ChakraUILink>
+          <ChakraUILink mr={7} onClick={() => setLoginFlg(true)}>
+            Sign-in
+          </ChakraUILink>
         </NextLink>
         <NextLink href={'#'}>
           <ChakraUILink mr={7}>Sign-up</ChakraUILink>
+        </NextLink>
+      </Flex>
+
+      <Flex
+        display={loginFlg ? 'flex' : 'none'}
+        align="center"
+        fontSize="xl"
+        fontWeight="bold"
+      >
+        <NextLink href={'AddNewSpending'}>
+          <ChakraUILink mr={7}>家計簿を入力</ChakraUILink>
+        </NextLink>
+        <NextLink href={'/'}>
+          <ChakraUILink mr={7} onClick={() => setLoginFlg(false)}>
+            Logout
+          </ChakraUILink>
         </NextLink>
       </Flex>
     </Flex>
