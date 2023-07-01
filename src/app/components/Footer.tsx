20164dae48ead32f38b23e78f374522855612cb0
tomichans <kaito39smile@gmail.com>
2023-07-01 14:11:45 +0900

    ♻️ インデントをスペース4つに変更

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,19 +2,19 @@
 import { Box, Text } from '@chakra-ui/react'
 
 export default function Footer() {
-  return (
-    <Box
-      w="100%"
-      h="70px"
-      position="fixed"
-      left="0"
-      bottom="0"
-      color="white"
-      bgColor="black"
-      display="flex"
-      alignItems="center"
-    >
-      <Text mx="auto">Copyright © 2023 tomichans</Text>
-    </Box>
-  )
+    return (
+        <Box
+            w="100%"
+            h="70px"
+            position="fixed"
+            left="0"
+            bottom="0"
+            color="white"
+            bgColor="black"
+            display="flex"
+            alignItems="center"
+        >
+            <Text mx="auto">Copyright © 2023 tomichans</Text>
+        </Box>
+    )
 }
