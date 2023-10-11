125a2205b3305696b39bb3eaec46b2d278e436a5
tomichans <kaito39smile@gmail.com>
2023-10-12 07:51:12 +0900

    ♻️ 関数名や型の修正、順番の入れ替え

--- src/app/hooks/useAppLogin.tsx
+++ src/app/hooks/useAppLogin.tsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import { ILoginInfo } from '../../interfaces/AppLogin/interfaces'
+import { ChangeEvent, useState } from 'react';
+import { ILoginInfo } from '../../interfaces/AppLogin/interfaces';
 
 export const useAppLogin = () => {
     //#region 変数関連
 
     /** ログイン情報デフォルト値 */
-    const ILoginInfoDefault: ILoginInfo = {
+    const loginInfoDefault: ILoginInfo = {
         /** ユーザー名 */
         username: '',
         /** パスワード */
         password: '',
-    }
+    };
 
     // ログイン情報
-    const [loginInfo, setFormValues] = useState(ILoginInfoDefault)
+    const [loginInfo, setLoginInfo] = useState(loginInfoDefault);
     // ログイン成功/失敗判定
-    const [isLoginSuccess, setIsLoginSuccess] = useState(false)
+    const [isLoginSuccess, setIsLoginSuccess] = useState(false);
     // ユーザー名正/誤判定
-    const [isCorrectUsername, setIsCorrectUsername] = useState(false)
+    const [isCorrectUsername, setIsCorrectUsername] = useState(false);
     // パスワード正/誤判定
-    const [isCorrectPassword, setIsCorrectPassword] = useState(false)
+    const [isCorrectPassword, setIsCorrectPassword] = useState(false);
     // 初期状態であるか判定
-    const [isInit, setIsInit] = useState(true)
+    const [isInit, setIsInit] = useState(true);
 
     //#endregion
 
@@ -29,53 +29,50 @@ export const useAppLogin = () => {
      * 各テキストボックス更新時処理
      * @param e イベントハンドラ
      */
-    const changeText = (e: any) => {
-        const { name, value } = e.target
-        setFormValues({ ...loginInfo, [name]: value })
-    }
+    const changeText = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setLoginInfo({ ...loginInfo, [name]: value });
+    };
 
     /**
      * ログインボタン押下時処理
-     * @param e イベントハンドラ
      */
-    const onClickLogin = (e: any) => {
+    const onClickLogin = () => {
         // 初期状態ではなくなる
-        const isInit = false
-        setIsInit(isInit)
+        setIsInit(false);
         // ログイン情報の入力チェック
-        setIsLoginSuccess(chackLoginInfo())
-
-        console.log(isInit)
-        console.log(isLoginSuccess)
-    }
+        setIsLoginSuccess(chackLoginInfo());
+    };
 
     //#region チェック処理関連
 
     /**
-     * 入力されたログイン情報のチェック処理
+     * ユーザー名のチェック処理
      * @returns true = チェックOK, false = チェックNG
      */
-    function chackLoginInfo(): boolean {
-        const correctUsername = checkUsername()
-        const correctPassword = checkPassword()
-        setIsCorrectUsername(correctUsername)
-        setIsCorrectPassword(correctPassword)
-        return correctUsername && correctPassword
-    }
+    const checkUsername = () => {
+        return loginInfo.username?.length > 0;
+    };
+
     /**
-     * 入力されたユーザー名のチェック処理
+     * パスワードのチェック処理
      * @returns true = チェックOK, false = チェックNG
      */
-    function checkUsername(): boolean {
-        return loginInfo.username?.length > 0
-    }
+    const checkPassword = () => {
+        return loginInfo.password?.length > 0;
+    };
+
     /**
-     * 入力されたパスワードのチェック処理
-     * @returns true = チェックOK, false = チェックNG
+     * ログイン情報のチェック処理
+     * @returns true:チェックOK, false:チェックNG
      */
-    function checkPassword(): boolean {
-        return loginInfo.password?.length > 0
-    }
+    const chackLoginInfo = () => {
+        const isExistUserName = checkUsername();
+        const isExistPassword = checkPassword();
+        setIsCorrectUsername(isExistUserName);
+        setIsCorrectPassword(isExistPassword);
+        return isExistUserName && isExistPassword;
+    };
 
     //#endregion
 
@@ -87,5 +84,5 @@ export const useAppLogin = () => {
         isInit,
         changeText,
         onClickLogin,
-    }
-}
+    };
+};
